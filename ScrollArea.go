4830101af84158b0d220ff34f1b771a3b368a561
Richard Wilkes <wilkes@me.com>
2016-07-12 11:31:50 -0700

    Add ScrollArea

---
+++ ScrollArea.go
@@ -0,0 +1,130 @@
+// Copyright (c) 2016 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
+package ui
+
+// ScrollArea provides a widget that can hold another widget and show it through a scrollable
+// viewport.
+type ScrollArea struct {
+	Block
+	hBar    *ScrollBar
+	vBar    *ScrollBar
+	view    *Block
+	content Widget
+}
+
+// NewScrollArea creates a new ScrollArea with the specified block as its content. The content may
+// be nil.
+func NewScrollArea(content Widget) *ScrollArea {
+	sa := &ScrollArea{}
+	sa.view = NewBlock()
+	sa.view.SetBackground(TextBackgroundColor)
+	sa.AddChild(sa.view)
+	sa.hBar = NewScrollBar(true, sa)
+	sa.vBar = NewScrollBar(false, sa)
+	sa.content = content
+	if content != nil {
+		sa.view.AddChild(content)
+	}
+	newScrollLayout(sa)
+	return sa
+}
+
+// View returns the view port block.
+func (sa *ScrollArea) View() *Block {
+	return sa.view
+}
+
+// Content returns the content block.
+func (sa *ScrollArea) Content() Widget {
+	return sa.content
+}
+
+// SetContent sets the content block, replacing any existing one.
+func (sa *ScrollArea) SetContent(content Widget) {
+	if sa.content != nil {
+		sa.content.RemoveFromParent()
+	}
+	sa.content = content
+	if content != nil {
+		sa.view.AddChildAtIndex(content, 0)
+	}
+	sa.SetNeedLayout(true)
+	sa.Repaint()
+}
+
+// LineScrollAmount implements Pager and Scrollable.
+func (sa *ScrollArea) LineScrollAmount(horizontal, towardsStart bool) float32 {
+	if sa.content != nil {
+		if s, ok := interface{}(sa.content).(Pager); ok {
+			return s.LineScrollAmount(horizontal, towardsStart)
+		}
+	}
+	return 16
+}
+
+// PageScrollAmount implements Pager and Scrollable.
+func (sa *ScrollArea) PageScrollAmount(horizontal, towardsStart bool) float32 {
+	if sa.content != nil {
+		if s, ok := interface{}(sa.content).(Pager); ok {
+			return s.PageScrollAmount(horizontal, towardsStart)
+		}
+	}
+	size := sa.view.Size()
+	if horizontal {
+		return size.Width
+	}
+	return size.Height
+}
+
+// ScrolledPosition implements Scrollable.
+func (sa *ScrollArea) ScrolledPosition(horizontal bool) float32 {
+	if sa.content == nil {
+		return 0
+	}
+	loc := sa.content.Location()
+	if horizontal {
+		return -loc.X
+	}
+	return -loc.Y
+}
+
+// SetScrolledPosition implements Scrollable.
+func (sa *ScrollArea) SetScrolledPosition(horizontal bool, position float32) {
+	if sa.content != nil {
+		loc := sa.content.Location()
+		if horizontal {
+			loc.X = -position
+		} else {
+			loc.Y = -position
+		}
+		sa.content.SetLocation(loc)
+	}
+}
+
+// VisibleSize implements Scrollable.
+func (sa *ScrollArea) VisibleSize(horizontal bool) float32 {
+	size := sa.view.Size()
+	if horizontal {
+		return size.Width
+	}
+	return size.Height
+}
+
+// ContentSize implements Scrollable.
+func (sa *ScrollArea) ContentSize(horizontal bool) float32 {
+	if sa.content == nil {
+		return 0
+	}
+	size := sa.content.Size()
+	if horizontal {
+		return size.Width
+	}
+	return size.Height
+}
---
+++ ScrollLayout.go
@@ -0,0 +1,93 @@
+// Copyright (c) 2016 by Richard A. Wilkes. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, version 2.0. If a copy of the MPL was not distributed with
+// this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// This Source Code Form is "Incompatible With Secondary Licenses", as
+// defined by the Mozilla Public License, version 2.0.
+
+package ui
+
+type scrollLayout struct {
+	sa *ScrollArea
+}
+
+func newScrollLayout(sa *ScrollArea) *scrollLayout {
+	layout := &scrollLayout{sa: sa}
+	sa.SetLayout(layout)
+	return layout
+}
+
+// Sizes implements the Layout interface.
+func (layout *scrollLayout) Sizes(hint Size) (min, pref, max Size) {
+	_, hBarSize, _ := ComputeSizes(layout.sa.hBar, NoLayoutHintSize)
+	_, vBarSize, _ := ComputeSizes(layout.sa.vBar, NoLayoutHintSize)
+	min.Width = vBarSize.Width * 2
+	min.Height = hBarSize.Height * 2
+	if layout.sa.content != nil {
+		_, pref, _ = ComputeSizes(layout.sa.content, hint)
+	}
+	if border := layout.sa.view.Border(); border != nil {
+		insets := border.Insets()
+		min.AddInsets(insets)
+		pref.AddInsets(insets)
+		max.AddInsets(insets)
+	}
+	return min, pref, DefaultLayoutMaxSize(pref)
+}
+
+// Layout implements the Layout interface.
+func (layout *scrollLayout) Layout() {
+	_, hBarSize, _ := ComputeSizes(layout.sa.hBar, NoLayoutHintSize)
+	_, vBarSize, _ := ComputeSizes(layout.sa.vBar, NoLayoutHintSize)
+	needHBar := false
+	needVBar := false
+	var contentSize Size
+	if layout.sa.content != nil {
+		contentSize = layout.sa.content.Size()
+	}
+	bounds := layout.sa.LocalInsetBounds()
+	visibleSize := bounds.Size
+	var viewInsets Insets
+	if border := layout.sa.view.Border(); border != nil {
+		viewInsets = border.Insets()
+	}
+	visibleSize.SubtractInsets(viewInsets)
+	if visibleSize.Width < contentSize.Width {
+		visibleSize.Height -= hBarSize.Height
+		needHBar = true
+	}
+	if visibleSize.Height < contentSize.Height {
+		visibleSize.Width -= vBarSize.Width
+		needVBar = true
+	}
+	if !needHBar && visibleSize.Width < contentSize.Width {
+		visibleSize.Height -= hBarSize.Height
+		needHBar = true
+	}
+	if needHBar {
+		if layout.sa.hBar.parent == nil {
+			layout.sa.AddChild(&layout.sa.hBar.Block)
+		}
+	} else {
+		layout.sa.hBar.RemoveFromParent()
+	}
+	if needVBar {
+		if layout.sa.vBar.parent == nil {
+			layout.sa.AddChild(&layout.sa.vBar.Block)
+		}
+	} else {
+		layout.sa.vBar.RemoveFromParent()
+	}
+	visibleSize.AddInsets(viewInsets)
+	layout.sa.view.SetBounds(Rect{Point: bounds.Point, Size: visibleSize})
+	if needHBar {
+		hBarSize.Width = visibleSize.Width
+		layout.sa.hBar.SetBounds(Rect{Point: Point{X: bounds.X, Y: bounds.Y + visibleSize.Height}, Size: hBarSize})
+	}
+	if needVBar {
+		vBarSize.Height = visibleSize.Height
+		layout.sa.vBar.SetBounds(Rect{Point: Point{X: bounds.X + visibleSize.Width, Y: bounds.Y}, Size: vBarSize})
+	}
+}
