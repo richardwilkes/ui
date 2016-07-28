d451ce80b7a9ddef98a03313d706d2e541e154db
Richard Wilkes <wilkes@me.com>
2016-07-27 23:53:16 -0700

    Add comments

--- event/Target.go
+++ event/Target.go
@@ -9,7 +9,10 @@
 
 package event
 
+// Target marks objects that can be the target of an event.
 type Target interface {
+	// EventHandlers returns the handler mappings for this Target.
 	EventHandlers() *Handlers
+	// ParentTarget returns the parent of this Target, or nil.
 	ParentTarget() Target
 }
