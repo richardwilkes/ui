db39a53ff31da8208db9074c1ec725bcb9213767
Richard Wilkes <wilkes@me.com>
2016-08-20 16:47:06 -0700

    Implement some of the Graphics api for Linux

--- draw/Graphics_linux.go
+++ draw/Graphics_linux.go
@@ -20,11 +20,11 @@ import (
 import "C"
 
 func (gc *graphics) platformSave() {
-	// RAW: Implement platformSave for Linux
+	C.cairo_save(gc.gc)
 }
 
 func (gc *graphics) platformRestore() {
-	// RAW: Implement platformRestore for Linux
+	C.cairo_restore(gc.gc)
 }
 
 func (gc *graphics) platformSetOpacity(opacity float32) {
@@ -32,7 +32,7 @@ func (gc *graphics) platformSetOpacity(opacity float32) {
 }
 
 func (gc *graphics) platformSetFillColor(color color.Color) {
-	// RAW: Implement platformSetFillColor for Linux
+	C.cairo_set_source_rgba(gc.gc, C.double(color.RedIntensity()), C.double(color.GreenIntensity()), C.double(color.BlueIntensity()), C.double(color.AlphaIntensity()))
 }
 
 func (gc *graphics) platformSetStrokeColor(color color.Color) {
@@ -40,15 +40,20 @@ func (gc *graphics) platformSetStrokeColor(color color.Color) {
 }
 
 func (gc *graphics) platformSetStrokeWidth(width float32) {
-	// RAW: Implement platformSetStrokeWidth for Linux
+	C.cairo_set_line_width(gc.gc, C.double(width))
 }
 
 func (gc *graphics) platformFillRect(bounds geom.Rect) {
-	// RAW: Implement platformFillRect for Linux
+	C.cairo_rectangle(gc.gc, C.double(bounds.X), C.double(bounds.Y), C.double(bounds.Width), C.double(bounds.Height))
+	C.cairo_fill(gc.gc)
 }
 
 func (gc *graphics) platformStrokeRect(bounds geom.Rect) {
-	// RAW: Implement platformFillRect for Linux
+	color := gc.StrokeColor()
+	C.cairo_set_source_rgba(gc.gc, C.double(color.RedIntensity()), C.double(color.GreenIntensity()), C.double(color.BlueIntensity()), C.double(color.AlphaIntensity()))
+	C.cairo_rectangle(gc.gc, C.double(bounds.X), C.double(bounds.Y), C.double(bounds.Width), C.double(bounds.Height))
+	C.cairo_stroke(gc.gc)
+	gc.platformSetFillColor(gc.FillColor())
 }
 
 func (gc *graphics) platformFillEllipse(bounds geom.Rect) {
@@ -60,39 +65,54 @@ func (gc *graphics) platformStrokeEllipse(bounds geom.Rect) {
 }
 
 func (gc *graphics) platformFillPath() {
-	// RAW: Implement platformFillPath for Linux
+	C.cairo_fill(gc.gc)
 }
 
 func (gc *graphics) platformFillPathEvenOdd() {
-	// RAW: Implement platformFillPathEvenOdd for Linux
+	current := C.cairo_get_fill_rule(gc.gc)
+	if current != C.CAIRO_FILL_RULE_EVEN_ODD {
+		C.cairo_set_fill_rule(gc.gc, C.CAIRO_FILL_RULE_EVEN_ODD)
+	}
+	C.cairo_fill(gc.gc)
+	if current != C.CAIRO_FILL_RULE_EVEN_ODD {
+		C.cairo_set_fill_rule(gc.gc, current)
+	}
 }
 
 func (gc *graphics) platformStrokePath() {
-	// RAW: Implement platformStrokePath for Linux
+	color := gc.StrokeColor()
+	C.cairo_set_source_rgba(gc.gc, C.double(color.RedIntensity()), C.double(color.GreenIntensity()), C.double(color.BlueIntensity()), C.double(color.AlphaIntensity()))
+	C.cairo_stroke(gc.gc)
+	gc.platformSetFillColor(gc.FillColor())
 }
 
 func (gc *graphics) platformFillAndStrokePath() {
-	// RAW: Implement platformFillAndStrokePath for Linux
+	C.cairo_fill_preserve(gc.gc)
+	gc.platformStrokePath()
 }
 
 func (gc *graphics) platformBeginPath() {
-	// RAW: Implement platformBeginPath for Linux
+	C.cairo_new_path(gc.gc)
 }
 
 func (gc *graphics) platformClosePath() {
-	// RAW: Implement platformClosePath for Linux
+	C.cairo_close_path(gc.gc)
 }
 
 func (gc *graphics) platformMoveTo(x, y float32) {
-	// RAW: Implement platformMoveTo for Linux
+	C.cairo_move_to(gc.gc, C.double(x), C.double(y))
 }
 
 func (gc *graphics) platformLineTo(x, y float32) {
-	// RAW: Implement platformLineTo for Linux
+	C.cairo_line_to(gc.gc, C.double(x), C.double(y))
 }
 
 func (gc *graphics) platformArc(cx, cy, radius, startAngleRadians, endAngleRadians float32, clockwise bool) {
-	// RAW: Implement platformArc for Linux
+	if clockwise {
+		C.cairo_arc(gc.gc, C.double(cx), C.double(cy), C.double(radius), C.double(startAngleRadians), C.double(endAngleRadians))
+	} else {
+		C.cairo_arc_negative(gc.gc, C.double(cx), C.double(cy), C.double(radius), C.double(startAngleRadians), C.double(endAngleRadians))
+	}
 }
 
 func (gc *graphics) platformArcTo(x1, y1, x2, y2, radius float32) {
@@ -100,31 +120,51 @@ func (gc *graphics) platformArcTo(x1, y1, x2, y2, radius float32) {
 }
 
 func (gc *graphics) platformCurveTo(cp1x, cp1y, cp2x, cp2y, x, y float32) {
-	// RAW: Implement platformCurveTo for Linux
+	C.cairo_curve_to(gc.gc, C.double(cp1x), C.double(cp1y), C.double(cp2x), C.double(cp2y), C.double(x), C.double(y))
 }
 
 func (gc *graphics) platformQuadCurveTo(cpx, cpy, x, y float32) {
-	// RAW: Implement platformQuadCurveTo for Linux
+	var x0, y0 C.double
+	C.cairo_get_current_point(gc.gc, &x0, &y0)
+	x1 := C.double(cpx)
+	y1 := C.double(cpy)
+	xx := C.double(x)
+	yy := C.double(y)
+	C.cairo_curve_to(gc.gc, 2.0/3.0*x1+1.0/3.0*x0, 2.0/3.0*y1+1.0/3.0*y0, 2.0/3.0*x1+1.0/3.0*xx, 2.0/3.0*y1+1.0/3.0*yy, xx, yy)
 }
 
 func (gc *graphics) platformAddPath(path *geom.Path) {
-	// RAW: Implement platformAddPath for Linux
+	path.Apply(gc.gc)
 }
 
 func (gc *graphics) platformClip() {
-	// RAW: Implement platformClip for Linux
+	C.cairo_clip(gc.gc)
 }
 
-func (gc *graphics) platformClipEvenOd() {
-	// RAW: Implement platformClipEvenOd for Linux
+func (gc *graphics) platformClipEvenOdd() {
+	current := C.cairo_get_fill_rule(gc.gc)
+	if current != C.CAIRO_FILL_RULE_EVEN_ODD {
+		C.cairo_set_fill_rule(gc.gc, C.CAIRO_FILL_RULE_EVEN_ODD)
+	}
+	C.cairo_clip(gc.gc)
+	if current != C.CAIRO_FILL_RULE_EVEN_ODD {
+		C.cairo_set_fill_rule(gc.gc, current)
+	}
 }
 
 func (gc *graphics) platformClipRect(bounds geom.Rect) {
-	// RAW: Implement platformClipRect for Linux
+	C.cairo_rectangle(gc.gc, C.double(bounds.X), C.double(bounds.Y), C.double(bounds.Width), C.double(bounds.Height))
+	C.cairo_clip(gc.gc)
 }
 
 func (gc *graphics) platformDrawLinearGradient(gradient *Gradient, sx, sy, ex, ey float32) {
-	// RAW: Implement platformDrawLinearGradient for Linux
+	pattern := C.cairo_pattern_create_linear(C.double(sx), C.double(sy), C.double(ex), C.double(ey))
+	for _, one := range gradient.Stops {
+		C.cairo_pattern_add_color_stop_rgba(pattern, C.double(one.Location), C.double(one.Color.RedIntensity()), C.double(one.Color.GreenIntensity()), C.double(one.Color.BlueIntensity()), C.double(one.Color.AlphaIntensity()))
+	}
+	C.cairo_set_source(gc.gc, pattern)
+	C.cairo_paint(gc.gc)
+	C.cairo_pattern_destroy(pattern)
 }
 
 func (gc *graphics) platformDrawRadialGradient(gradient *Gradient, scx, scy, startRadius, ecx, ecy, endRadius float32) {
@@ -136,17 +176,18 @@ func (gc *graphics) platformDrawImageInRect(img *Image, bounds geom.Rect) {
 }
 
 func (gc *graphics) platformDrawString(x, y float32, str string) {
-	// RAW: Implement platformDrawString for Linux
+	C.cairo_move_to(gc.gc, C.double(x), C.double(y+14)) // RAW: Hack until actual font code is written
+	C.cairo_show_text(gc.gc, C.CString(str))
 }
 
 func (gc *graphics) platformTranslate(x, y float32) {
-	// RAW: Implement platformTranslate for Linux
+	C.cairo_translate(gc.gc, C.double(x), C.double(y))
 }
 
 func (gc *graphics) platformScale(x, y float32) {
-	// RAW: Implement platformScale for Linux
+	C.cairo_scale(gc.gc, C.double(x), C.double(y))
 }
 
 func (gc *graphics) platformRotate(angleInRadians float32) {
-	// RAW: Implement platformRotate for Linux
+	C.cairo_rotate(gc.gc, C.double(angleInRadians))
 }
