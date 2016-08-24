d90cbb7d0ac648479d6c491b7008d0d40ad196c0
Richard Wilkes <wilkes@me.com>
2016-08-23 17:29:29 -0700

    Choose some reasonable default fonts for Windows and Linux

--- font/System_windows.go
+++ font/System_windows.go
@@ -10,15 +10,15 @@
 package font
 
 func init() {
-	// RAW: Define the system fonts for windows
-	User = NewFont("")
-	UserMonospaced = NewFont("")
-	System = NewFont("")
-	EmphasizedSystem = NewFont("")
-	SmallSystem = NewFont("")
-	SmallEmphasizedSystem = NewFont("")
-	Views = NewFont("")
-	Label = NewFont("")
-	Menu = NewFont("")
-	MenuCmdKey = NewFont("")
+	// RAW: Try to determine what the user set in system preferences...
+	User = NewFont("Sans 12")
+	UserMonospaced = NewFont("Monospace 10")
+	System = NewFont("Sans 13")
+	EmphasizedSystem = NewFont("Sans Bold 13")
+	SmallSystem = NewFont("Sans 11")
+	SmallEmphasizedSystem = NewFont("Sans Bold 11")
+	Views = NewFont("Sans 12")
+	Label = NewFont("Sans 10")
+	Menu = NewFont("Sans 14")
+	MenuCmdKey = NewFont("Sans 14")
 }
