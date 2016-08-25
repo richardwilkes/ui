c1b6c79f755e1475ca810985039167ebd3ba0f53
Richard Wilkes <wilkes@me.com>
2016-08-25 07:22:12 -0700

    Add comments

--- draw/Paint.go
+++ draw/Paint.go
@@ -29,13 +29,13 @@ const (
 type PaintKind int
 
 const (
-	// Pixels outside of the pattern are fully transparent.
+	// PaintModeNone pixels outside of the pattern are fully transparent.
 	PaintModeNone PaintMode = C.CAIRO_EXTEND_NONE
-	// The pattern is tiled by repeating.
+	// PaintModeRepeat the pattern is tiled by repeating.
 	PaintModeRepeat PaintMode = C.CAIRO_EXTEND_REPEAT
-	// The pattern is tiled by reflecting at the edges.
+	// PaintModeReflect the pattern is tiled by reflecting at the edges.
 	PaintModeReflect PaintMode = C.CAIRO_EXTEND_REFLECT
-	// Pixels outside of the pattern copy the closest pixel from the source.
+	// PaintModePad pixels outside of the pattern copy the closest pixel from the source.
 	PaintModePad PaintMode = C.CAIRO_EXTEND_PAD
 )
 
@@ -60,18 +60,23 @@ const (
 // FilterMode defines the type of filtering to be used when reading pixel values.
 type FilterMode int
 
+// Paint holds patterns used when filling and stroking paths.
 type Paint struct {
 	pattern *C.cairo_pattern_t
 }
 
+// NewColorPaint creates a new Paint from a color.
 func NewColorPaint(color color.Color) Paint {
 	return Paint{pattern: C.cairo_pattern_create_rgba(C.double(color.RedIntensity()), C.double(color.GreenIntensity()), C.double(color.BlueIntensity()), C.double(color.AlphaIntensity()))}
 }
 
+// NewImagePaint creates a new Paint from an image.
 func NewImagePaint(img *Image) Paint {
 	return Paint{pattern: C.cairo_pattern_create_for_surface(img.surface)}
 }
 
+// NewLinearGradientPaint creates a new Paint from a gradient that is spread across the specified
+// line.
 func NewLinearGradientPaint(gradient *Gradient, sx, sy, ex, ey float64) Paint {
 	pattern := C.cairo_pattern_create_linear(C.double(sx), C.double(sy), C.double(ex), C.double(ey))
 	for _, one := range gradient.Stops {
@@ -80,6 +85,8 @@ func NewLinearGradientPaint(gradient *Gradient, sx, sy, ex, ey float64) Paint {
 	return Paint{pattern: pattern}
 }
 
+// NewRadialGradientPaint creates a new Paint from a gradient that radiates from one circle to
+// another.
 func NewRadialGradientPaint(gradient *Gradient, scx, scy, startRadius, ecx, ecy, endRadius float64) Paint {
 	pattern := C.cairo_pattern_create_radial(C.double(scx), C.double(scy), C.double(startRadius), C.double(ecx), C.double(ecy), C.double(endRadius))
 	for _, one := range gradient.Stops {
@@ -88,36 +95,44 @@ func NewRadialGradientPaint(gradient *Gradient, scx, scy, startRadius, ecx, ecy,
 	return Paint{pattern: pattern}
 }
 
+// PaintMode returns the PaintMode of this Paint.
 func (p Paint) PaintMode() PaintMode {
 	return PaintMode(C.cairo_pattern_get_extend(p.pattern))
 }
 
+// SetPaintMode sets the PaintMode of this Paint.
 func (p Paint) SetPaintMode(mode PaintMode) {
 	C.cairo_pattern_set_extend(p.pattern, C.cairo_extend_t(mode))
 }
 
+// FilterMode returns the FilterMode of this Paint.
 func (p Paint) FilterMode() FilterMode {
 	return FilterMode(C.cairo_pattern_get_filter(p.pattern))
 }
 
+// SetFilterMode sets the FilterMode of this Paint.
 func (p Paint) SetFilterMode(mode FilterMode) {
 	C.cairo_pattern_set_filter(p.pattern, C.cairo_filter_t(mode))
 }
 
+// Matrix returns the Matrix of this Paint.
 func (p Paint) Matrix() *geom.Matrix {
 	var matrix C.cairo_matrix_t
 	C.cairo_pattern_get_matrix(p.pattern, &matrix)
 	return fromCairoMatrix(&matrix)
 }
 
+// SetMatrix sets the Matrix of this Paint.
 func (p Paint) SetMatrix(matrix *geom.Matrix) {
 	C.cairo_pattern_set_matrix(p.pattern, toCairoMatrix(matrix))
 }
 
+// Kind returns the PaintKind of this Paint.
 func (p Paint) Kind() PaintKind {
 	return PaintKind(C.cairo_pattern_get_type(p.pattern))
 }
 
+// Dispose releases the underlying resources used by this Paint.
 func (p Paint) Dispose() {
 	C.cairo_pattern_destroy(p.pattern)
 	p.pattern = nil
