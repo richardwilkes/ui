cb5c8d61d5882cbd3bba30d2b86fc775ce5f77c5
Richard Wilkes <wilkes@me.com>
2018-10-30 07:48:34 -0700

    Fix typo

--- display/display_linux.go
+++ display/display_linux.go
@@ -6,8 +6,8 @@ func platformMainDisplayBounds() geom.Rect {
 	// RAW: Replace with implementation
 	return geom.Rect{
 		Size: geom.Size{
-			Width: 1024,
-			Height; 768,
+			Width:  1024,
+			Height: 768,
 		},
 	}
 }
